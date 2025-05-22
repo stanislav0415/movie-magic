1abee53e456823d93c0c731fbd110c9dd7b065a5
stanislav0415 <149686416+stanislav0415@users.noreply.github.com>
2025-05-22 12:01:56 +0300

    add handlebars

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import express from 'express'
+import handlebars from 'express-handlebars'
+
+
 
 const app = express();
 
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs',
+}))
+
+
+app.set('view endigne', 'hbs')
+
 app.get('/', (req, res) => {
-    res.send('it works')
+    res.render('home', {layout: false})
+
 });
 
-app.listen(5000, () => console.log('Server is listening on http://localhost:5500....'));
+app.listen(5000, () => console.log('Server is listening on http://localhost:5000....'));
 
