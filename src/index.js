6651e4910296c043a904e5cb08927f9db02da3d1
stanislav0415 <149686416+stanislav0415@users.noreply.github.com>
2025-05-22 13:59:04 +0300

    add main ect layouts

--- src/index.js
+++ src/index.js
@@ -17,9 +17,13 @@ app.set('view engine', 'hbs');
 app.set('views', './src/views');
 
 app.get('/', (req, res) => {
-    res.render('home', {layout: false})
-
+    res.render('home');
 });
+app.get('/about', (req, res) => {
+    res.render('about' );
+});
+
+
 
 app.listen(5000, () => console.log('Server is listening on http://localhost:5000....'));
 
