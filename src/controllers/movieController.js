cddbc2695e9f374ec5d4c06b29424bbcc0d133de
stanislav0415 <149686416+stanislav0415@users.noreply.github.com>
2025-05-29 18:12:52 +0300

    fixed issues

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -1,32 +1,40 @@
-import express from 'express';
-import movieService from '../services/movieService';
+import express from 'express'
+import movieService from '../services/movieService.js';
 
 const movieController = express.Router();
 
 movieController.get('/create', (req, res) => {
     res.render('create');
-})
+});
 
-movieController.post('/create', (req, res) => {
+movieController.post('/create', async (req, res) => {
     const newMovie = req.body;
 
-    movieService.create(newMovie);
+    // Save Movie
+    await movieService.create(newMovie);
 
+    // Redirect to home page
     res.redirect('/');
+});
 
-    movieController.get('/movieId/details', (req, res) => {
-        const movieId = req.params.movieId;
+movieController.get('/:movieId/details', (req, res) => {
+    // Get movie id from params
+    const movieId = req.params.movieId;
+    
+    // Get movie data
+    const movie = movieService.getOne(movieId);
 
-        const movie = movieService.getOne(movieId);
+    res.render('details', { movie });
+});
 
-        res.render('details')
-    })
-    movieController.get('/search', (req, res) => {
+movieController.get('/search', (req, res) => {
+    // Get querystring
+    const filter = req.query;
+    
+    // Get all movies
+    const movies = movieService.getAll(filter);
 
-        const movies = movieService.getAll()
-        
-        res.render('search', { movies })
-    })
-})
+    res.render('search', { movies });
+});
 
 export default movieController;
--- src/index.js
+++ src/index.js
@@ -1,32 +1,41 @@
 import express from 'express'
 import handlebars from 'express-handlebars'
+
 import homeController from './controllers/homeController.js';
 import movieController from './controllers/movieController.js';
 
-
-
+// Init express instance
 const app = express();
 
-app.use(express.static('./src/public'))
+// Add static middleware
+app.use(express.static('./src/public'));
 
+// Add body parser
 app.use(express.urlencoded());
 
+// Add and config view engine
 app.engine('hbs', handlebars.engine({
     extname: 'hbs',
-}))
+    helpers: {
+        showRating(rating) {
+            return '★'.repeat(Math.floor(rating));
+            // return '&#x2605;'.repeat(Math.floor(rating));
+        }
+    }
+}));
 
-
+// Set default engine
 app.set('view engine', 'hbs');
 
+// Set default view folder
 app.set('views', './src/views');
 
+// Config routes
 app.use(homeController);
-app.use('/movies',movieController);
+app.use('/movies', movieController);
 app.all('*url', (req, res) => {
-    res.render('404')
-})
+    res.render('404');
+});
 
-
-
+// Start express web server
 app.listen(5000, () => console.log('Server is listening on http://localhost:5000....'));
-
