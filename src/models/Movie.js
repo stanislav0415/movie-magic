abef346ffb4de0847dfb60324f92cc96685bad48
stanislav0415 <149686416+stanislav0415@users.noreply.github.com>
2025-05-31 12:08:33 +0300

    movies added description

--- src/models/Movie.js
+++ src/models/Movie.js
@@ -1,18 +1,49 @@
 import { Schema, model } from "mongoose";
 
+const maxYearAllowed = new Data().getFullYear() + 5
 const MovieSchema = new Schema({
-    title: String,
-    category: String,
-    genre: String,
-    director: String,
-    year: Number,
-    imageUrl: String,
-    rating: Number,
-    description: String,
+  title: { 
+    type: String, 
+    required: [true, 'title is required'] 
 
+},
+  category: { 
+    type: String, 
+    required: [true, 'category is required'] 
+},
+  genre: { 
+    type: String, 
+    required: [true, 'genre is required'] 
+},
+  director: { 
+    type: String, 
+    required: [true, 'director is required'] 
+},
+  year: { 
+    type: Number, 
+    required: true,
+    min: 1970,
+    max: [maxYearAllowed,`Year can not be larger ${maxYearAllowed}`]
+
+ },
+  imageUrl: {
+     type: String,
+     required: [true, 'imageUrl is required'] 
+    
+    },
+  rating: { 
+    type: Number, 
+    required: [true, 'rating is required'] 
+
+},
+  description: { 
+    type: String, 
+    required: [true, 'description is required'],
+    maxLength: [100, 'Description is too long!']
+
+},
 });
 
-const Movie = model('Movie', MovieSchema);
+const Movie = model("Movie", MovieSchema);
 
-export default Movie
-
+export default Movie;
