e85937d7241ebac14634db056ea062536db8cf31
stanislav0415 <149686416+stanislav0415@users.noreply.github.com>
2025-06-05 08:51:39 +0300

    add users user coontroller

---
+++ src/controllers/userController.js
@@ -0,0 +1,10 @@
+import { Router } from "express";
+
+const userController = Router();
+
+
+userController.get('/register', (req, res) => {
+    res.send('Register page!');
+})
+
+export default userController;
